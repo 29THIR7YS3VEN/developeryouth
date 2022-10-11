0587295d5604a6a1107cc4c9b9cf86ccf8725d97
Emmanuel Alabi <alabiemmanuelxxix@outlook.com>
2022-10-11 13:03:02 +0200

    Update app_database_init.py

--- app_database_init.py
+++ app_database_init.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('main.db')
+connection = sqlite3.connect('database.db')
 with open('database_schema.sql') as f:
     connection.executescript(f.read())
 cur = connection.cursor()
