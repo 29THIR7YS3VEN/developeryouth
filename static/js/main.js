cc7d5183431f0c0d6f1e0b47db710530f612168a
dukk0 <bytex07@proton.me>
2022-10-19 22:38:08 +0530

    #1 file structure

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-// Deal with dropdowns
-function openDropdown(){
-    document.getElementById('dropdown-menu').style.display = 'block';
-}
-function closeDropdown(){
-    document.getElementById('dropdown-menu').style.display = 'hidden';
-}
