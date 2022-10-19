3eb5da3fd51106cd4556f858fe8c6ac6a6f75f6e
Emmanuel A <alabiemmanuelxxix@outlook.com>
2022-10-19 14:44:43 +0200

    Update database_schema.sql

--- database_schema.sql
+++ database_schema.sql
@@ -1,27 +1,32 @@
+/* file containing sql commands to initialize the web-app's main database*/
+
+-- Users, to handle user accounts, signup, signin.
 CREATE TABLE users (
   -- account details
-  user_id INTEGER PRIMARY_KEY,
-  username TEXT,
-  email TEXT,
-  display_name TEXT,
+  user_id INTEGER PRIMARY_KEY, -- Unique ID will be assigned to each user
+  username TEXT, -- Username as chosen by user
+  email TEXT, -- Email as inputed by user
+  display_name TEXT, -- Display name as inputted by user
   password TEXT, -- ideally will be hashed using werkzeug
-  date_of_birth TEXT,
+  date_of_birth TEXT, -- We want to restrict the platform so this will be better
   
   -- administrative info
-  status TEXT,
+  status TEXT, -- account status (unverified, active, banned, etc.)
   
   -- customizable user info
-  profile_photo_path TEXT,
-  banner_photo_path TEXT,
+  profile_photo_path TEXT, --will contain the path linking to the profile picture
+  banner_photo_path TEXT, --will contain the path linking to the banner picture
   user_bio TEXT 
 );
 
+-- Handle all projects created on the platform
 CREATE TABLE projects (
-  project_id INTEGER PRIMARY_KEY,
-  project_name TEXT,
-  project_description TEXT,
-  project_photo TEXT,
-  creator TEXT,
+  project_id INTEGER PRIMARY_KEY, --Each project will be assigned a unique id
+  project_name TEXT, --As inputted by the creator
+  project_description TEXT, --as inputted by creator
+  project_photo TEXT, --""
+  creator_id INTEGER, -- Will be used in the programme to identify the creator
+  creator TEXT, -- creator's username/display name
   contributors TEXT,
-  date_created TEXT
+  date_created TEXT -- date created.
 );
